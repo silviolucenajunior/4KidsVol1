86a20c2b5f1b1edb6729aef848f9bee0a30400f4
Silvio Lucena Junior <silvio_lucena_junior@hotmail.com>
2016-08-31 23:26:44 -0300

    improve(GamePain) Change method of drawLine to simple lineTo

--- phaser/states/StateGamePaint.js
+++ phaser/states/StateGamePaint.js
@@ -2,7 +2,7 @@ function StateGamePaint() {
     this.bitmapdata = undefined;
     this.colors = undefined;
     this.actualColor = 0;
-    this.explosions = [];
+    this.points = [];
     this.particleEmitter = null;
 };
 
@@ -11,35 +11,52 @@ StateGamePaint.prototype = {
     preload: function(){
         game.load.image('bubble', '/phaser/assets/bubble.png');
         game.load.image('particle', '/phaser/assets/purpledot.gif');
-
     },
     _startParticleEmitter: function(){
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.particleEmitter = game.add.emitter(50, 50, 100);
         this.particleEmitter.makeParticles('particle');
         this.particleEmitter.gravity = 200;
-        this.particleEmitter.start(false, 2000, 20);
+        this.particleEmitter.start(false, 1000, 20);
+        this.particleEmitter.on = false;
     },
     create: function(){
         this._startParticleEmitter();
         this.colors = Phaser.Color.HSVColorWheel();
         this.bitmapdata = game.add.bitmapData(800, 600);
         game.add.sprite(0, 0, this.bitmapdata);
-        game.input.addMoveCallback(this.paint, this);
-        game.input.onTap.add(this.addExplosion, this);
         game.input.onDown.add(this.enableParticleEmitter, this);
         game.input.onUp.add(this.disableParticleEmitter, this);
+        game.input.addMoveCallback(this.paint, this);
     },
     update: function(){
+        this.bitmapdata.clear();
+        this._drawPointsAsLine();
+    },
+    _drawPointsAsLine: function(){
+        for (var i = 0, count = this.points.length; i < count; i++){
+            this.points[i].lifeTime -= 1;
+            this.drawLine(i > 0 ? this.points[i-1] : this.points[i], this.points[i]);
+        }
+    },
+    drawLine: function(fromPoint, toPoint){
+        this.bitmapdata.ctx.strokeStyle = "white";
+        this.bitmapdata.ctx.beginPath();
+        this.bitmapdata.ctx.moveTo(fromPoint.x, fromPoint.y);
+        this.bitmapdata.ctx.lineTo(toPoint.x, toPoint.y);
+        this.bitmapdata.ctx.lineWidth = 3;
+        this.bitmapdata.ctx.stroke();
+        this.bitmapdata.ctx.closePath();
+        this.bitmapdata.render();
+        //this.bitmapdata.refreshBuffer();
     },
     enableParticleEmitter: function(point){
+        this.particleEmitter.on = true;
         console.log("Enable Particle Emitter");
     },
     disableParticleEmitter: function(pointer) {
+        this.particleEmitter.on = false;
         console.log("Disable Particle Emitter");
-    },
-    addExplosion: function(point, x, y){
-        console.log("Add New Explosion at " + x + "< " + y);
     },
     paint: function(pointer, x, y){
         this.particleEmitter.emitX = x;
@@ -47,8 +64,13 @@ StateGamePaint.prototype = {
         //this.particleEmitter.x = x;
         //this.particleEmitter.y = y;
         if (pointer.isDown){
-            this.bitmapdata.circle(x, y, 16, this.colors[this.actualColor].rgba);
-            this.actualColor = game.math.wrapValue(this.actualColor, 1, 359);
+            this.points.push({
+                x: x,
+                y: y,
+                lifeTime: 1000
+            });
+            //this.bitmapdata.circle(x, y, 2, this.colors[this.actualColor].rgba);
+            //this.actualColor = game.math.wrapValue(this.actualColor, 1, 359);
         }
     }
 };
